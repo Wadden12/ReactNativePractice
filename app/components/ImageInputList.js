20dc844db6ddaca51e7aadff884cf5a6be85f889
Mike Wadden <mike.wadden@keyin.com>
2023-01-31 11:48:57 -0330

    update changes

---
+++ app/components/ImageInputList.js
@@ -0,0 +1,42 @@
+import { StyleSheet, Text, View, ScrollView } from "react-native";
+import React, { useRef } from "react";
+import ImageInput from "./ImageInput";
+
+function ImageInputList({ imageUris = [], onRemoveImage, onAddImage }) {
+  const scrollView = useRef();
+
+  return (
+    <View>
+      <ScrollView
+        // Scroll View takes all available space you need wrap in a view to limit it
+        // Wrap in View to contain it
+        ref={scrollView}
+        horizontal
+        onContentSizeChange={() => scrollView.current.scrollToEnd()}
+      >
+        <View style={styles.container}>
+          {imageUris.map((uri) => (
+            <View style={styles.image} key={uri}>
+              <ImageInput
+                imageUri={uri}
+                onChangeImage={() => onRemoveImage(uri)}
+              />
+            </View>
+          ))}
+          <ImageInput onChangeImage={(uri) => onAddImage(uri)} />
+        </View>
+      </ScrollView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+  },
+  image: {
+    marginRight: 10,
+  },
+});
+
+export default ImageInputList;
