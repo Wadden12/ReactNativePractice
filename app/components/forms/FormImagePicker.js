b0e0b65136034b9508b582d3eb68168a9858ef1e
Mike Wadden <mike.wadden@keyin.com>
2023-02-01 10:48:35 -0330

    Starting to add nav to the project

--- app/components/forms/FormImagePicker.js
+++ app/components/forms/FormImagePicker.js
@@ -1,12 +1,34 @@
-import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { useFormikContext } from "formik";
 
-function FormImagePicker(props) {
-  return <View style={styles.container}></View>;
+import ImageInputList from "../ImageInputList";
+import ErrorMessage from "./ErrorMessage";
+
+function FormImagePicker({ name }) {
+  const { errors, setFieldValue, touched, values } = useFormikContext();
+  const imageUris = values[name];
+
+  const handleAdd = (uri) => {
+    setFieldValue(name, [...imageUris, uri]);
+  };
+
+  const handleRemove = (uri) => {
+    setFieldValue(
+      name,
+      imageUris.filter((imageUri) => imageUri !== uri)
+    );
+  };
+
+  return (
+    <>
+      <ImageInputList
+        imageUris={values[name]}
+        onAddImage={handleAdd}
+        onRemoveImage={handleRemove}
+      />
+      <ErrorMessage error={errors[name]} visible={touched[name]} />
+    </>
+  );
 }
-
-const styles = StyleSheet.create({
-  container: {},
-});
 
 export default FormImagePicker;
--- app/config/notes.js
+++ app/config/notes.js
@@ -144,3 +144,96 @@ const categories = [
 
 // on change alternative example
 // onChangeText={(text) => setPassword(text)}
+
+<ImageInputList
+  imageUris={imageUris}
+  // uri => handleAdd(uri)} is the same as below
+  onAddImage={handleAdd}
+  onRemoveImage={handleRemove}
+/>;
+
+const Link = () => {
+  const navigation = useNavigation();
+
+  return (
+    <Button title="Click" onPress={() => navigation.navigate("TweetDetails")} />
+  );
+};
+
+const Tweets = ({ navigation }) => (
+  <Screen>
+    <Text>Tweets</Text>
+    <Button
+      title="View Tweet"
+      onPress={() => navigation.navigate("TweetDetails", { id: 1 })}
+    />
+  </Screen>
+);
+
+const TweetDetails = ({ route }) => (
+  <Screen>
+    <Text>Tweets Details {route.params.id}</Text>
+  </Screen>
+);
+
+const Stack = createStackNavigator();
+const FeedNavigator = () => (
+  // can be changed globally with the stack Navigator
+
+  <Stack.Navigator
+    screenOptions={{
+      headerStyle: { backgroundColor: "dodgerblue" },
+      headerTintColor: "white",
+    }}
+    initialRouteName="Tweets"
+  >
+    <Stack.Screen
+      name="Tweets"
+      component={Tweets}
+      // unquie to the componet
+      options={{
+        headerStyle: { backgroundColor: "tomato" },
+        headerTintColor: "white",
+        // headerShown: false,
+      }}
+    />
+    <Stack.Screen
+      name="TweetDetails"
+      component={TweetDetails}
+      // one way
+      options={{ title: "Tweet Details" }}
+      // how to set Dynamically
+      // options={(route) => ({ title: route.params.id })}
+    />
+  </Stack.Navigator>
+);
+
+const Account = () => (
+  <Screen>
+    <Text>Account</Text>
+  </Screen>
+);
+
+const Tab = createBottomTabNavigator();
+
+const TabNavigator = () => (
+  <Tab.Navigator
+    tabBarOptions={{
+      activeBackgroundColor: "tomato",
+      activeTintColor: "white",
+      inactiveBackgroundColor: "#eee",
+      inactiveTintColor: "black",
+    }}
+  >
+    <Tab.Screen
+      name="Feed"
+      component={FeedNavigator}
+      options={{
+        tabBarIcon: ({ size, color }) => (
+          <MaterialCommunityIcons name="home" size={size} color={color} />
+        ),
+      }}
+    />
+    <Tab.Screen name="Account" component={Account} />
+  </Tab.Navigator>
+);
